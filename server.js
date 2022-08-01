c4621800ef721a6b6268371e079868e5706af675
vishal <vishalgoud890@gmail.com>
2022-08-01 20:27:33 +0530

    first GET route is done

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+
+
+const mockUserData = [
+    {name: "akash"},
+    {name: "Rathod"}
+]
+
+app.get('/users', function(rep, res){
+    res.json({
+        success:true,
+        message: 'successfully got users. Nice',
+        users: mockUserData
+    })
+});
+
 app.listen(8000,function(){
     console.log("server is running");
-})
+});
